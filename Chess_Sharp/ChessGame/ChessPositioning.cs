0a1932b2f490ba4082f9f3ed8c687e7d65fd2f52
Heitor Lopes <hlcs082@gmail.com>
2022-09-13 10:22:32 +0100

    Creating the Chess positioning and notation method

---
+++ Chess_Sharp/ChessGame/ChessPositioning.cs
@@ -0,0 +1,27 @@
+﻿using Chess_Sharp.ChessBoard;
+
+
+namespace Chess_Sharp.ChessGame
+{
+    internal class ChessPositioning
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public ChessPositioning (char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position toChessPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+        public override string ToString()
+        {
+            return $"{Column}{Row}";
+        }
+
+    }
+}
--- Chess_Sharp/Program.cs
+++ Chess_Sharp/Program.cs
@@ -8,22 +8,10 @@ namespace Chess_Sharp
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            ChessPositioning pos = new ChessPositioning('a', 1);
 
-            try
-            {
-                board.AddPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.AddPiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.AddPiece(new King(board, Color.Black), new Position(0, 9));
-
-
-                Screen.PrintBoard(board);
-                Console.ReadLine();
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toChessPosition());
         }
     }
 }
